b79f233cb5012f3d99e35e433d8ad7f588a06da3
awesomecat <782453374@qq.com>
2021-12-15 00:57:36 +0800

    delete PersonInfo class

--- src/main/java/com/awesomecat/emailhelp/service/impl/MailMessageParserImpl.java
+++ src/main/java/com/awesomecat/emailhelp/service/impl/MailMessageParserImpl.java
@@ -1,7 +1,6 @@
 package com.awesomecat.emailhelp.service.impl;
 
 import com.awesomecat.emailhelp.bo.MailInfo;
-import com.awesomecat.emailhelp.bo.PersonInfo;
 import com.awesomecat.emailhelp.service.MailMessageParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
