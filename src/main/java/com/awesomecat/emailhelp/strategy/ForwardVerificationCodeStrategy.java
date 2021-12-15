d0e8ec53cffe677f222c0ce3b3e60130bcb4d6fc
awesomecat <782453374@qq.com>
2021-12-15 11:51:31 +0800

    verification code filter

--- src/main/java/com/awesomecat/emailhelp/strategy/ForwardVerificationCodeStrategy.java
+++ src/main/java/com/awesomecat/emailhelp/strategy/ForwardVerificationCodeStrategy.java
@@ -3,6 +3,7 @@ package com.awesomecat.emailhelp.strategy;
 import com.awesomecat.emailhelp.bo.MailInfo;
 import com.awesomecat.emailhelp.service.MailHandlerService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -11,8 +12,9 @@ import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 转发验证码策略
@@ -26,27 +28,57 @@ public class ForwardVerificationCodeStrategy implements ReceiveMailHandleStrateg
 
     /** 默认接收人 */
     // TODO: 2021/12/15 后期移至配置中心或数据库
-    private static final String[] RECIPIENT_STRING_ARRAY = {"", "", ""};
-    /** 主题 */
-    private static final String SUBJECT = "";
+    private static final String[] RECIPIENT_STRING_ARRAY = {"xxx@qq.com", "xxx@163.com"};
+    /** 发送主题 */
+    private static final String SUBJECT = "bilibili 验证码转发";
+    /** bilibili 地址 */
+    private static final String BILIBILI_ADDRESS = "verify@service.bilibili.com";
+    /** bilibili 标识 */
+    private static final String BILIBILI_SIGN = "哔哩哔哩";
+    /** 验证码标识 */
+    private static final String VERIFICATION_CODE_SIGN = "验证码";
+    /** 验证码正则 */
+    private static final Pattern VERIFICATION_CODE_PATTERN = Pattern.compile("<strong>(\\d{6})</strong>");
 
     @Resource
     private MailHandlerService mailHandlerService;
 
     @Override
     public void doHandle(MailInfo receiveMailInfo) {
+        // bilibili 验证
+        if (!isBilibiliVerificationCode(receiveMailInfo)) {
+            return;
+        }
+
         log.info("forward verification code start");
         try {
             MailInfo sendMailInfo = new MailInfo();
             // 发件人就是上一次的接收人
             sendMailInfo.setFrom(receiveMailInfo.getTo().get(0));
             sendMailInfo.setTo(this.getRecipientList());
-            sendMailInfo.setSubject(SUBJECT);
-            sendMailInfo.setContent(this.getVerificationCode(receiveMailInfo));
+            String verificationCode = this.getVerificationCode(receiveMailInfo);
+            sendMailInfo.setContent(verificationCode + "悠着点，不要乱登！");
+            sendMailInfo.setSubject(SUBJECT + "：" + verificationCode);
             mailHandlerService.sendEmail(sendMailInfo);
         } catch (MessagingException e) {
             log.error("forward verification code error", e);
         }
+    }
+
+    private boolean isBilibiliVerificationCode(MailInfo receiveMailInfo) {
+        // 不包含验证码，不发送邮件
+        if (!receiveMailInfo.getContent().contains(VERIFICATION_CODE_SIGN)) {
+            return false;
+        }
+
+        // 发件人验证
+        InternetAddress from = (InternetAddress) receiveMailInfo.getFrom();
+        if (BILIBILI_ADDRESS.equals(from.getAddress()) || BILIBILI_SIGN.equals(from.getPersonal())) {
+            return true;
+        }
+
+        // 主题验证
+        return receiveMailInfo.getSubject().contains(BILIBILI_SIGN);
     }
 
     /**
@@ -58,8 +90,12 @@ public class ForwardVerificationCodeStrategy implements ReceiveMailHandleStrateg
      * @date 2021/12/15 0:45
      */
     private String getVerificationCode(MailInfo receiveMailInfo) {
-        // TODO: 2021/12/15 获取验证码
-        return null;
+        Matcher matcher = VERIFICATION_CODE_PATTERN.matcher(receiveMailInfo.getContent());
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return StringUtils.EMPTY;
     }
 
     public List<Address> getRecipientList() {
